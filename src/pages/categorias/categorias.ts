fef77cadeb7a252c18713f13143e6397eba8e92c
SilvioCristiano <sivuca1@gmail.com>
2018-04-05 23:34:41 -0300

    Mostrando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,6 +17,8 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items : CategoriaDTO[];
 
   constructor(
     public navCtrl: NavController, 
@@ -24,14 +28,13 @@ export class CategoriasPage {
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
-    .subscribe(response => {
-      console.log(response);
-    },
-      error => {
-        console.log(error);
-      });
- 
+      .subscribe(response => {
+        this.items = response;
+      },
+      error => {});
   }
-  
+  showProdutos(categoria_id : string) {
+    this.navCtrl.push('ProdutosPage', {categoria_id: categoria_id});    
+  }
 
 }
